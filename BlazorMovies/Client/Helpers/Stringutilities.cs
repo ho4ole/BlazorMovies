79333f2a757098820941a0187012734d98a8f44c
ho4ole <daniilkuznetss@gmail.com>
2021-12-02 23:02:58 +0100

    0.1

---
+++ BlazorMovies/Client/Helpers/Stringutilities.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorMovies.Client.Helpers
+{
+    public class Stringutilities
+    {
+        public static string CustomToUpper(string name) 
+        { 
+          return _ = name.ToUpper();
+        }
+    }
+}
